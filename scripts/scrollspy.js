f52ac733ab43298dc2c0f95db523cc0bebae9be3
Aliics <Aliics@hotmail.com>
2018-07-27 07:35:31 +1200

    added url change on scrollspy update. fixed bug where couldn't get divs past index 1. colored test divs. :)))

--- scripts/scrollspy.js
+++ scripts/scrollspy.js
@@ -8,42 +8,46 @@ document.addEventListener( "scroll", checkScreenPosition );
 document.addEventListener( "resize", checkScreenPosition );
 
 function checkScreenPosition() {
+	oldSection = currentSection;
+
+	var anyWereSelected = false;
+
 	for ( var s = 0; s < sections.length; s ++ ) {
 		var section = sections[ s ];
 		var sectionRect = section.getBoundingClientRect();
 
-		var isAlreadyActive = section === currentSection;
-
-		if ( withinViewport( section ) && !isAlreadyActive ) {
-			oldSection = currentSection;
+		if ( withinViewport( section ) ) {
+			anyWereSelected = true;
 			currentSection = section;
-			break;
 		}
 	}
 
-	if ( currentSection == undefined )
+	if ( currentSection == undefined || oldSection == undefined ) {
 		return;
+	}
 
 	if ( oldSection != undefined ) 
 		if ( withinViewport( oldSection ) && oldSection.offsetTop > currentSection.offsetTop )
 			return;
 
-	removeOldNavigationButtons();
+	var oldSectionIdFormatted = oldSection.id + "-navigation";
+	var oldNavigationEquivalent = document.getElementById( oldSectionIdFormatted );
 
+	oldNavigationEquivalent.classList.remove( activeClassName );
+
+	if ( !anyWereSelected ) {
+		currentSection = undefined;
+		history.pushState( null, null, "" );
+		return;
+	}
+
+	var sectionIdHash = "#" + currentSection.id;
 	var sectionIdFormatted = currentSection.id + "-navigation";
 	var navigationEquivalent = document.getElementById( sectionIdFormatted );
 	
 	navigationEquivalent.classList.add( activeClassName );
-}
 
-function removeOldNavigationButtons() {
-	var oldNavigationButtons = document.getElementsByClassName( activeClassName );
-
-	for ( var o = 0; o < oldNavigationButtons.length; o ++ ) {
-		var oldNavigationButton = oldNavigationButtons[ o ];
-
-		oldNavigationButton.classList.remove( activeClassName );
-	}
+	history.pushState( null, null, sectionIdHash );
 }
 
 function withinViewport( element ) {
