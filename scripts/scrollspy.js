26397c291914d1a4156146511f1368f48d18aea7
Aliics <Aliics@hotmail.com>
2018-07-28 12:30:00 +1200

    did a lot of patching of elementInViewport ( previously withinViewport ). fixed most if not all glitches of scrollspy. made navigationbar stay on screen, even on scroll. added new css for index page. :))))))

--- scripts/scrollspy.js
+++ scripts/scrollspy.js
@@ -6,40 +6,42 @@ var activeClassName = "active-navigation-button";
 
 document.addEventListener( "scroll", checkScreenPosition );
 document.addEventListener( "resize", checkScreenPosition );
+document.addEventListener( "touchmove", checkScreenPosition );
 
 function checkScreenPosition() {
 	oldSection = currentSection;
 
-	var anyWereSelected = false;
+	var jumbotronElement = document.getElementsByClassName( "jumbotron" )[ 0 ];
+	var navigationBarElement = document.getElementsByClassName( "main-navigation" )[ 0 ];
+
+	navigationBarElement.style.position = elementInViewport( jumbotronElement ) ? "relative" : "fixed";
+	
+	var navigationBarSize = navigationBarElement.offsetHeight; 
+	var shouldAddPadding = navigationBarElement.style.position == "fixed";
+
+	sections[ 0 ].style.paddingTop = shouldAddPadding ? navigationBarSize + "px" : "0px";
 
 	for ( var s = 0; s < sections.length; s ++ ) {
 		var section = sections[ s ];
 		var sectionRect = section.getBoundingClientRect();
 
-		if ( withinViewport( section ) ) {
+		if ( elementInViewport( section ) ) {
 			anyWereSelected = true;
 			currentSection = section;
 		}
 	}
 
-	if ( currentSection == undefined || oldSection == undefined ) {
+	if ( currentSection == undefined || oldSection == undefined )
 		return;
-	}
 
 	if ( oldSection != undefined ) 
-		if ( withinViewport( oldSection ) && oldSection.offsetTop > currentSection.offsetTop )
+		if ( elementInViewport( oldSection ) && oldSection.offsetTop > currentSection.offsetTop )
 			return;
 
 	var oldSectionIdFormatted = oldSection.id + "-navigation";
 	var oldNavigationEquivalent = document.getElementById( oldSectionIdFormatted );
 
 	oldNavigationEquivalent.classList.remove( activeClassName );
-
-	if ( !anyWereSelected ) {
-		currentSection = undefined;
-		history.pushState( null, null, "" );
-		return;
-	}
 
 	var sectionIdHash = "#" + currentSection.id;
 	var sectionIdFormatted = currentSection.id + "-navigation";
@@ -50,6 +52,10 @@ function checkScreenPosition() {
 	history.pushState( null, null, sectionIdHash );
 }
 
-function withinViewport( element ) {
-	return element.offsetTop < window.pageYOffset + window.innerHeight;
+function elementInViewport( element ) {
+	var bottomWithinViewport = element.offsetTop + element.offsetHeight >= window.pageYOffset &&
+				   element.offsetTop + element.offsetHeight <= window.pageYOffset + window.innerHeight;
+	var topWithinViewport = element.offsetTop <= window.pageYOffset + window.innerHeight && 
+				element.offsetTop >= window.pageYOffset;
+	return topWithinViewport || bottomWithinViewport;
 }
